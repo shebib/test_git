07a4e3ca8d50cc0c436e958f81c4410dd346e55f
Daniel Shebib <shebib@usc.edu>
2018-01-10 09:22:34 -0800

    Lab1 folder

---
+++ shebib_CSCI201L_Lab1/src/shebib_CSCI201L_Lab1/HelloWorld.java
@@ -0,0 +1,13 @@
+package shebib_CSCI201L_Lab1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String s = "Hello World!";
+		s=null;
+		s.length();
+		System.out.println("Hello World!");
+
+	}
+
+}
